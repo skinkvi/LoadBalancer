b67e5aeb247cf076c439c268004cd033abe23e55
skinkvi <skinkvi228aye@gmail.com>
2024-09-08 12:25:36 +0500

    comm

--- servers/server1.go
+++ servers/server1.go
@@ -4,22 +4,30 @@ import (
 	"fmt"
 	"net/http"
 	"sync"
+
+	"load_balancer/internal/config"
 )
 
 func main() {
+	c, err := config.LoadConfig("config/config.yaml")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	var wg sync.WaitGroup
-	for i := 0; i < 4; i++ {
+	for _, server := range c.Servers {
 		wg.Add(1)
-		go func(port string) {
+		go func(url string) {
 			defer wg.Done()
 
 			http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-				fmt.Fprintf(w, "Hello from server %s", port)
+				fmt.Fprintf(w, "Hello from server %s", url)
 			})
 
-			fmt.Printf("Server running on %s\n", port)
-			http.ListenAndServe(port, nil)
-		}(":" + fmt.Sprintf("%d", 8080+i))
+			fmt.Printf("Server running on %s\n", url)
+			http.ListenAndServe(url[7:], nil)
+		}(server.URL)
 	}
 
 	wg.Wait()
